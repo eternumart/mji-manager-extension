cf0a39d4e6de8bd3c6e29e4b0c1d15ce79727e36
EternumArt <levashushkov@yandex.ru>
2026-02-05 11:25:06 +0300

    delete files

--- МЖИ_files/utlPrint.js
+++
@@ -1,8 +0,0 @@
-function printConsolidatedDoc(button) {
-  var dt = domNodeFirstChild(domNodeParent(button,4).rows[0].cells[1]).getElementsByTagName('INPUT')[0].value;
-  parent.Container.query(button,'printConsolidatedDocCB',dt);
-}
-
-function printConsolidatedDocCB(reportPath) {
-  actionHREF(Form.pathBI + reportPath, '_blank');
-}
